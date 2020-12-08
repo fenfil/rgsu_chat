347ae73967db98a79758cfccbaff585083ab978f
fenfil <vladimir.zaykovskiy@ya.ru>
2020-12-08 12:49:37 +0300

    add new features

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 const morgan = require("morgan");
+const fs = require("fs");
+const crypto = require("crypto");
 
 const app = express();
 const server = http.createServer(app);
@@ -9,26 +11,96 @@ const io = socketio(server);
 
 const users = new Set();
 const messages = [];
+const isUserValid = name => name.match(/^[a-zA-Z_]{4,255}$/);
+const shouldSaveLogs = process.argv.includes("-l");
+const adminPassword = "cats";
+const secret = "many";
+const adminPasswordHash = hash(adminPassword);
+
+if (shouldSaveLogs) console.log("Will save logs");
+
+const savemsg = msg => {
+  if (shouldSaveLogs)
+    fs.writeFile("./logs.txt", `${msg}\n`, { flag: "a" }, err => {
+      if (err) console.error(err);
+    });
+};
+function hash(text) {
+  return crypto
+    .createHmac("sha256", secret)
+    .update(text)
+    .digest("hex");
+}
 
 io.on("connect", socket => {
-  let name;
+  let name, isAdmin;
   socket.on("join", ({ name: n }, cb) => {
+    if (!isUserValid(n))
+      return cb(
+        "Name should be at least 4 letters length and 255 as max and contain only a-Z and _"
+      );
+    if (users.has(n)) return cb("User with such name have already joined");
     name = n;
     users.add(n);
-    cb(messages);
-  });
-  socket.on("msg", (text, cb) => {
-    if (text.startsWith("/")) {
-      if (text == "/room") {
-        return cb(Array.from(users));
+    socket.join(`user:${n}`);
+    cb(null, messages);
+    io.emit("msg", { text: `New user connected! now online: ${users.size}` });
+
+    socket.on("msg", (text, cb) => {
+      if (text.startsWith("/")) {
+        if (text == "/room") {
+          return cb(Array.from(users));
+        } else if (text.match(/\/msg .+ .+/)) {
+          text = text.slice(text.indexOf(" ") + 1);
+          const user = text.slice(0, text.indexOf(" "));
+          const msg = text.slice(text.indexOf(" ") + 1);
+          if (name === user || !users.has(user)) return cb("no such user");
+          io.to(`user:${name}`).emit("msg", { text: msg, user: name });
+          io.to(`user:${user}`).emit("msg", { text: msg, user: name });
+          savemsg(`[private ${name}]: ${text}`);
+        } else if (text.startsWith("/rename ")) {
+          const newName = text.slice(8).trim();
+          if (!isUserValid(newName))
+            return cb(
+              "Name should be at least 4 letters length and 255 as max and contain only a-Z and _"
+            );
+          if (users.has(newName))
+            return cb("User with such name have already joined");
+          socket.leave(`user:${name}`);
+          socket.join(`user:${newName}`);
+          users.delete(name);
+          users.add(newName);
+          name = newName;
+          cb();
+          socket.emit("msg", { text: `Your new name is ${newName}` });
+        } else if (text.startsWith("/auth ")) {
+          const password = text.slice(6).trim();
+          if (adminPasswordHash !== hash(password)) return cb("Wrong password");
+          isAdmin = true;
+          cb("Now you are admin");
+        } else if (text.startsWith("/disconnect ")) {
+          if (!isAdmin) return cb("Not authorized");
+          const user = text.slice(12).trim();
+          for (const socketId in io.sockets.sockets) {
+            const socket = io.sockets.sockets[socketId];
+            if (socket.rooms[`user:${user}`]) {
+              socket.disconnect();
+              cb("User has been disconnected");
+              break;
+            }
+          }
+        } else {
+          cb("Unknown command");
+        }
+        return;
       }
-      return cb("Unknown command");
-    }
-    messages.push({ text, name });
-    io.emit("msg", { text, name });
-  });
-  socket.on("disconnect", () => {
-    users.delete(name);
+      savemsg(`[${name}]: ${text}`);
+      messages.push({ text, name });
+      io.emit("msg", { text, name });
+    });
+    socket.on("disconnect", () => {
+      users.delete(name);
+    });
   });
 });
 
